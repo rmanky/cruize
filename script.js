0858677d77366e68f5d684419824fed143c48efc
Robear Mankaryous <rmankaryous12@gmail.com>
2019-01-25 17:59:35 -0500

    push me daddy

--- script.js
+++ script.js
@@ -15,11 +15,299 @@ let config = {
 console.log("Firebase: Begin!");
 firebase.initializeApp(config);
 
-const createMap = ({ lat, lng }) => {
+let directionsService, directionsDisplay, pos;
+
+document.getElementById("search-bar").addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        let destination = document.getElementById("search-bar").value;
+        console.log(destination);
+        displayRoute(destination);
+    }
+});
+
+const createMap = (latLng) => {
     return new google.maps.Map(document.getElementById('map'), {
-        center: {lat, lng},
+        center: latLng,
         disableDefaultUI: true,
-        zoom: 20
+        zoom: 20,
+        styles: [
+            {
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#1d2c4d"
+                    }
+                ]
+            },
+            {
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#8ec3b9"
+                    }
+                ]
+            },
+            {
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#1a3646"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.country",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#4b6878"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#64779e"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.province",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#4b6878"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.man_made",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#334e87"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.natural",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#023e58"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#283d6a"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#6f9ba5"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#1d2c4d"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.business",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#023e58"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#3C7680"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#304a7d"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#98a5be"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#1d2c4d"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#2c6675"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#255763"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#b0d5ce"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#023e58"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#98a5be"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#1d2c4d"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#283d6a"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.station",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#3a4762"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#0e1626"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#4e6d70"
+                    }
+                ]
+            }
+        ]
     });
 };
 
@@ -29,14 +317,14 @@ const createMap = ({ lat, lng }) => {
  * @param {Object} position
  * @return {Object}
  */
-const createMarker = ({ map, position }) => {
+const createMarker = ({map, latLng}) => {
     let icon = {
         url: 'https://openclipart.org/image/2400px/svg_to_png/192456/car-black.png', // url
-        scaledSize: new google.maps.Size(35,75),
-        origin: new google.maps.Point(0,0), // origin
+        scaledSize: new google.maps.Size(35, 75),
+        origin: new google.maps.Point(0, 0), // origin
         anchor: new google.maps.Point(35 / 2, 75 / 2) // anchor
     };
-    return new google.maps.Marker({map, icon, position});
+    return new google.maps.Marker({map, icon, latLng});
 };
 
 /**
@@ -45,7 +333,10 @@ const createMarker = ({ map, position }) => {
  * @param {Object} [onError]
  * @return {number}
  */
-const trackLocation = ({ onSuccess, onError = () => { } }) => {
+const trackLocation = ({
+                           onSuccess, onError = () => {
+    }
+                       }) => {
     if ('geolocation' in navigator === false) {
         return onError(new Error('Geolocation is not supported by your browser.'));
     }
@@ -78,41 +369,50 @@ const getPositionErrorMessage = code => {
  * Automatically called by the google maps API once it's loaded.
  */
 function init() {
-    const initialPosition = { lat: 59.32, lng: 17.84 };
-    const map = createMap(initialPosition);
-    const marker = createMarker({ map, position: initialPosition });
+    pos = new google.maps.LatLng(59.32, 17.84);
+    const map = createMap(pos);
+    const marker = createMarker({map, pos});
 
-    let directionsService = new google.maps.DirectionsService;
-    let directionsDisplay = new google.maps.DirectionsRenderer({preserveViewport: true, map: map});
-
-    displayRoute('Worcester, MA', 'Boston, MA', directionsService,
-        directionsDisplay);
+    directionsService = new google.maps.DirectionsService;
+    let polyOptions = ({
+        clickable: false,
+        strokeColor: 'blueviolet',
+        strokeWeight: 10,
+    });
+    directionsDisplay = new google.maps.DirectionsRenderer({
+        preserveViewport: true,
+        map: map,
+        suppressMarkers: true,
+        polylineOptions: polyOptions
+    });
 
     trackLocation({
-        onSuccess: ({ coords: { latitude: lat, longitude: lng } }) => {
-            marker.setPosition({ lat, lng });
-            displayRoute(new google.maps.LatLng(lat, lng), 'Boston, MA', directionsService, directionsDisplay);
-            map.panTo({ lat, lng });
+        onSuccess: ({coords: {latitude: lat, longitude: lng}}) => {
+            marker.setPosition({lat, lng});
+            map.panTo({lat, lng});
+            pos = new google.maps.LatLng(lat, lng);
         },
         onError: err => {
             console.log(`Error: ${err.message || getPositionErrorMessage(err.code)}`);
         }
     });
-
     directionsDisplay.setMap(map);
-    directionsDisplay.setPanel(document.getElementById('directionsPanel'));
 }
 
-function displayRoute(origin, destination, service, display) {
-    service.route({
-        origin: origin,
-        destination: destination,
-        travelMode: 'DRIVING'
-    }, function(response, status) {
-        if (status === 'OK') {
-            display.setDirections(response);
-        } else {
-            alert('Could not display directions due to: ' + status);
-        }
-    });
+function displayRoute(destination) {
+    if (directionsService != null && directionsDisplay != null) {
+        directionsService.route({
+            origin: pos,
+            destination: destination,
+            travelMode: 'DRIVING'
+        }, function (response, status) {
+            if (status === 'OK') {
+                directionsDisplay.setDirections(response);
+            } else {
+                alert('Could not display directions due to: ' + status);
+            }
+        });
+    } else {
+        console.error("DirectionsService or DirectionsDisplay was null")
+    }
 }

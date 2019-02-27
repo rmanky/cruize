0800039a776dbe508897ed53e1af5d827cdc5c61
rmanky <rmankaryous12@gmail.com>
2019-02-27 00:22:40 +0000

    idk did stuff

--- mapBoxMap.js
+++ mapBoxMap.js
@@ -34,15 +34,15 @@ mapBoxMap = (function () {
     function setDestinationMarker(destination) {
         destinationMarker.setLngLat(destination.coords);
         console.log(myPos);
-        map.fitBounds([myPos, destination.coords], {
-            padding: 100
-        });
     }
 
     return {
         setDestination: async function () {
             let destination = await new Destination(searchBar.value);
             await setDestinationMarker(destination);
+            map.fitBounds([myPos, destination.coords], {
+                padding: 100
+            });
             let geoJson = await new Route(myPos, destination.coords);
             if(map.getLayer('route') == undefined) {
                 map.addLayer({
